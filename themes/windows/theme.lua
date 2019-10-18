b6f1a6d42cf8f10c221b092b9f815b30d33b3470
Seth Barberee <seth.barberee@gmail.com>
2019-10-18 06:10:11 -0500

    new theme: windows. Basically trying to mock windows

---
+++ themes/windows/theme.lua
@@ -0,0 +1,299 @@
+---------------------------
+-- wal awesome theme --
+---------------------------
+local theme_assets = require("beautiful.theme_assets")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+local gfs = require("gears.filesystem")
+local themes_path = gfs.get_themes_dir()
+
+local awful = require('awful')
+local wibox = require('wibox')
+local gears = require('gears')
+local naughty = require('naughty')
+
+-- TODO find way to load from ~/.cache and not by symlinking to awesome
+-- directory
+local pywal = require("colors")
+
+local theme = {}
+
+theme.font          = "sans 9"
+
+-- {{{ Background
+theme.bg_normal     = pywal.background
+theme.bg_focus      = pywal.color11
+theme.bg_urgent     = pywal.color2
+theme.bg_minimize   = pywal.color1
+theme.bg_systray    = theme.bg_normal
+-- }}}
+-- {{{ Foreground
+theme.fg_normal     = pywal.foreground
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+-- }}}
+-- {{{ Border Settings
+theme.useless_gap   = 0
+theme.border_width  = dpi(1)
+theme.border_normal = theme.bg_normal
+theme.border_focus  = theme.bg_focus
+theme.border_marked = "#91231c"
+-- }}}
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
+-- tasklist_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- prompt_[fg|bg|fg_cursor|bg_cursor|font]
+-- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- {{{ Taglist Settings
+-- Generate taglist squares:
+local taglist_square_size = dpi(4)
+theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
+    taglist_square_size, theme.fg_normal
+)
+theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
+    taglist_square_size, theme.fg_normal
+)
+
+theme.taglist_bg_focus = pywal.color14 -- Pywal blue
+-- }}}
+
+--theme.tasklist_bg = "#ffffff"
+--theme.tasklist_bg_focus = "#ffffff"
+theme.tasklist_fg_focus = "#000000"
+
+theme.wibar_fg = "#ffffff"
+-- {{{ Notification Settings
+-- Variables set for theming notifications:
+-- notification_font
+-- notification_[bg|fg]
+-- notification_[width|height|margin]
+-- notification_[border_color|border_width|shape|opacity]
+
+theme.notification_fg = pywal.color14
+
+-- }}}
+-- {{{ Menu Theming 
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_height = dpi(15)
+theme.menu_width  = dpi(100)
+
+-- }}}
+-- {{{ Titlebar images
+-- Define the image to load
+theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
+theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+
+theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
+theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+-- }}}
+-- {{{ Wallpaper Settings
+-- Set wallpaper from wal. If we don't have one, set the default
+theme.wallpaper = pywal.wallpaper
+if theme.wallpaper == 'None' then
+    theme.wallpaper = themes_path .. "default/background.png"
+end
+-- }}}
+-- {{{ Layout images
+-- You can use your own layout icons like this:
+theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
+theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
+theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
+theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
+theme.layout_max = themes_path.."default/layouts/maxw.png"
+theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
+theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
+theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
+theme.layout_tile = themes_path.."default/layouts/tilew.png"
+theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
+theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
+theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
+theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
+theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
+theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
+theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+-- }}}
+
+-- Generate Awesome icon:
+theme.awesome_icon = theme_assets.awesome_icon(
+    theme.menu_height, theme.bg_focus, theme.fg_focus
+)
+
+-- Define the icon theme for application icons. If not set then the icons
+-- from /usr/share/icons and /usr/share/icons/hicolor will be used.
+theme.icon_theme = nil
+
+local search_text = wibox.widget.textbox()
+search_text.text = 'Search'
+local search_box = wibox.widget {
+    {
+        {
+            {
+                -- TODO add iconbox here
+                search_text,
+                layout = wibox.layout.fixed.horizontal,
+            },
+            left = 10,
+            right = 10,
+            widget = wibox.container.margin,
+        },
+        bg = theme.border_focus,
+        fg = theme.fg_focus,
+        widget = wibox.container.background,
+    },
+    layout = wibox.layout.fixed.horizontal,
+}
+-- Lets me left click to pull up rofi
+search_box:buttons(awful.util.table.join(awful.button({}, 1, function () awful.spawn('rofi -combi-modi window,drun,run -show combi -modi combi') end)))
+
+
+local tasklist_buttons = {
+    awful.button({ }, 1, function (c)
+        if c == client.focus then
+            c.minimized = true
+        else
+            c:emit_signal(
+            "request::activate",
+            "tasklist",
+            {raise = true}
+            )
+        end
+    end),
+    awful.button({ }, 3, function()
+        awful.menu.client_list({ theme = { width = 250 } })
+    end),
+    awful.button({ }, 4, function ()
+        awful.client.focus.byidx(1)
+    end),
+    awful.button({ }, 5, function ()
+        awful.client.focus.byidx(-1)
+    end)
+}
+
+
+-- {{{ Tasklist wibar
+local mytasklist = awful.widget.tasklist {
+    screen   = awful.screen.focused(),
+    filter   = awful.widget.tasklist.filter.currenttags,
+    buttons  = tasklist_buttons,
+    layout   = {
+        spacing_widget = {
+            {
+                forced_width  = 5,
+                forced_height = 24,
+                thickness     = 1,
+                color         = '#777777',
+                widget        = wibox.widget.separator
+            },
+            valign = 'center',
+            halign = 'center',
+            widget = wibox.container.place,
+        },
+        spacing = 1,
+        layout  = wibox.layout.fixed.horizontal
+    },
+    -- Notice that there is *NO* wibox.wibox prefix, it is a template,
+    -- not a widget instance.
+    widget_template = {
+        {
+            wibox.widget.base.make_widget(),
+            forced_height = 5,
+            id            = 'background_role',
+            widget        = wibox.container.background,
+        },
+        {
+            awful.widget.clienticon,
+            margins = 5,
+            widget  = wibox.container.margin
+        },
+        nil,
+        layout = wibox.layout.align.vertical,
+        -- TODO is there a click callback???
+        create_callback = function(self, c, index, objects) local tooltip = awful.tooltip({
+            objects = { self },
+            timer_function = function()
+                return c.name
+            end,
+        })
+        end,
+    },
+}
+
+local notif_wb = awful.wibar {
+    position = 'bottom',
+    height   = 48,
+    type = 'dock',
+    bg = 'transparent',
+    --visible  = client.instances() > 0, -- TODO maybe make it appear when we have clients
+}
+
+local left_layout = wibox.layout.fixed.horizontal()
+left_layout:add(search_box)
+
+local layout = wibox.widget {
+    {
+        left_layout,
+        {
+            mytasklist,
+            content_fill_horizontal = true,
+            widget = wibox.container.place,
+        },
+        wibox.widget.systray(),
+        layout = wibox.layout.align.horizontal,
+    },
+    top = theme.border_width,
+    color = theme.border_focus,
+    widget = wibox.container.margin,
+}
+notif_wb:set_widget(layout)
+--notif_wb:setup {
+--    left_layout,
+--    mytasklist,
+--    wibox.widget.systray(),
+--    layout = wibox.layout.align.horizontal
+--}
+
+
+
+-- TODO migrate to only show when we have clients
+--client.connect_signal("manage", function()
+--    notif_wb.visible = client.instances() > 0
+--end)
+
+-- }}}
+
+return theme
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80:foldmethod=marker:fdl=0
