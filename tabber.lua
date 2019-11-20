d5dc56298c56eef08efe7714ca3605546349c285
Seth Barberee <seth.barberee@gmail.com>
2019-11-19 18:25:35 -0600

    fixed to deal with multi-monitor

--- tabber.lua
+++ tabber.lua
@@ -3,56 +3,63 @@ local gears = require("gears")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 
-local client_popup = awful.popup {
-    widget = awful.widget.tasklist {
-        screen   = mouse.screen, -- TODO see if this change is better
-        filter   = awful.widget.tasklist.filter.currenttags,
-        buttons  = tasklist_buttons,
-        style    = {
-            shape = gears.shape.rounded_rect,
-        },
-        layout   = {
-            spacing = 5,
-            forced_num_rows = 1, -- wanna mirror windows
-            layout = wibox.layout.grid.horizontal
-        },
-        widget_template = {
-            {
-                {
-                    id     = 'clienticon',
-                    widget = awful.widget.clienticon,
-                },
-                margins = 4,
-                widget  = wibox.container.margin,
+client_popup = {}
+
+screen.connect_signal("request::desktop_decoration", function(s)
+    local si = s.index
+    client_popup[si] = awful.popup { widget = awful.widget.tasklist {
+            screen   = s, -- TODO see if this change is better
+            filter   = awful.widget.tasklist.filter.currenttags,
+            buttons  = tasklist_buttons,
+            style    = {
+                shape = gears.shape.rounded_rect,
             },
-            id              = 'background_role',
-            forced_width    = 48,
-            forced_height   = 48,
-            widget          = wibox.container.background,
-            create_callback = function(self, c, index, objects) --luacheck: no unused
-                self:get_children_by_id('clienticon')[1].client = c
-            end,
+            layout   = {
+                spacing = 5,
+                forced_num_rows = 1, -- wanna mirror windows
+                layout = wibox.layout.grid.horizontal
+            },
+            widget_template = {
+                {
+                    {
+                        id     = 'clienticon',
+                        widget = awful.widget.clienticon,
+                    },
+                    margins = 4,
+                    widget  = wibox.container.margin,
+                },
+                id              = 'background_role',
+                forced_width    = 48,
+                forced_height   = 48,
+                widget          = wibox.container.background,
+                create_callback = function(self, c, index, objects) --luacheck: no unused
+                    self:get_children_by_id('clienticon')[1].client = c
+                end,
+            },
         },
-    },
-    border_color = beautiful.border_focus,
-    border_width = 2,
-    ontop        = true,
-    visible      = false, -- don't show on startup
-    placement    = awful.placement.centered,
-    shape        = gears.shape.rounded_rect
-}
+        border_color = beautiful.border_focus,
+        border_width = 2,
+        ontop        = true,
+        visible      = false, -- don't show on startup
+        placement    = awful.placement.centered,
+        screen       = s,
+        shape        = gears.shape.rounded_rect
+    }
+end)
 
 local function start_key()
     if #mouse.screen.clients > 1 then -- only do this for when we have more than 1 client
         awful.client.focus.history.disable_tracking()
-        client_popup.visible = true
+        local s = mouse.screen.index
+        client_popup[s].visible = true
     end
 end
 
 local function stop_key()
     if #mouse.screen.clients > 1 then -- only do this when we have more than 1 client
         awful.client.focus.history.enable_tracking()
-        client_popup.visible = false
+        local s = mouse.screen.index
+        client_popup[s].visible = false
     end
 end
 
